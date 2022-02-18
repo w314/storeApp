62ac84197d448e186c480b6f8ae6cf4552ebc8a3
Piroska Werner <piroska.werner@gmail.com>
2022-02-18 11:58:00 -0600

    feat: Add handlers to all product model methods

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,20 +1,69 @@
 import express from 'express';
+import { request } from 'http';
 // import type and class of model this handler file handles
 import { Product, ProductStore } from '../models/product';
 
 const store = new ProductStore();
 
 // express handler function
-const index = async (req: express.Request, res: express.Response) => {
-  const products = await store.index();
+const index = async (_req: express.Request, res: express.Response) => {
+  try {
+    console.log(`request came in`);
+    // const products = await store.index();
+    // console.log(`Products:`)
+    // console.log(products)
     // res.json(products)
-    res.send('Products index route')
+    res.send('Products index route');
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
 };
 
-// const show = async(req: express.Request, res: express.Response, id: number) => {
-//     const product = await store.show(id)
-//     res.send(`Products show route`)
-// }
+const show = async (req: express.Request, res: express.Response) => {
+  try {
+    // console.log(`SHOW request with url: ${req.url}`)
+    const id = parseInt(req.params.id);
+    // const product = await store.show(id)
+    // res.json(product)
+    res.send(`Product show route, id of requested product: ${id}`);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const create = async (_req: express.Request, res: express.Response) => {
+  try {
+    res.send('Products CREATE route');
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const update = async (req: express.Request, res: express.Response) => {
+  try {
+    // console.log(`SHOW request with url: ${req.url}`)
+    const id = parseInt(req.params.id);
+    // const product = await store.show(id)
+    // res.json(product)
+    res.send(`Product UPDATE route, id of requested product: ${id}`);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+const destroy = async (req: express.Request, res: express.Response) => {
+  try {
+    const id = parseInt(req.params.id);
+    res.send(`Product DELETE route, id of requested product: ${id}`);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
 
 // to allow the handler above access to express method we create the function below,
 // that takes in an instance of express application object as a parameter.
@@ -22,7 +71,11 @@ const index = async (req: express.Request, res: express.Response) => {
 const productRoutes = (app: express.Application) => {
   // we call express method, that match our routes and
   // call the RESTful route handler to create a response
-  app.get('/product', index);
+  app.get('/products', index);
+  app.get('/products/:id', show);
+  app.post('/products', create);
+  app.put('/products/:id', update);
+  app.delete('/products/:id', destroy);
 };
 
 export default productRoutes;

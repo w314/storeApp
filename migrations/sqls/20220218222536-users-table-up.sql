5e8c0176b192219bfba938076686ae0387b3dd16
Piroska Werner <piroska.werner@gmail.com>
2022-03-29 08:57:24 -0500

    feat: Add usertype to migration and make username unnique

--- migrations/sqls/20220218222536-users-table-up.sql
+++ migrations/sqls/20220218222536-users-table-up.sql
@@ -1,8 +1,10 @@
 /* Replace with your SQL commands */
+CREATE TYPE usertype AS ENUM ('admin', 'regular');
 CREATE TABLE users (
-    username VARCHAR(100),
+    username VARCHAR(100) UNIQUE,
     firstname VARCHAR(100), 
     lastname VARCHAR(100), 
     password_digest VARCHAR, 
     id SERIAL PRIMARY KEY
+    user_type usertype
 );

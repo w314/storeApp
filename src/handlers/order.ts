b6f321f845e2de955f2a0d6013e4375c92705216
Piroska Werner <piroska.werner@gmail.com>
2022-04-19 08:42:51 -0500

    feat: Add POST orders/:userId/active and POST orders/:userId enpoints to orders handler

--- src/handlers/order.ts
+++ src/handlers/order.ts
@@ -18,15 +18,43 @@ const create = async (req:express.Request, res:express.Response) => {
     } catch (err) {
         res.status(500)
         res.json(err)
-        
     }
-
 }
+
+const activeOrder = async (req: express.Request, res: express.Response) =>  {
+
+    const userId = parseInt(req.params.userId)
+
+    try {
+        const activeOrder = await orderStore.activeOrder(userId)
+        res.json(activeOrder)
+    } catch (err) {
+        res.send(500)
+        res.json(err)
+    }
+}
+
+
+const orderList = async (req: express.Request, res: express.Response) => {
+
+    const userId = parseInt(req.params.userId)
+
+    try {
+        const orderList = await orderStore.orderList(userId)
+        res.json(orderList)
+    } catch (err) {
+        res.send(500)
+        res.json(err)
+    }
+}
+
 
 
 
 const orderRoutes = (app:express.Application) => {
     app.post('/orders', create)
+    app.get('/orders/:userId/active', activeOrder)
+    app.get('orders/:userId', orderList)
 }
 
 export default orderRoutes;
